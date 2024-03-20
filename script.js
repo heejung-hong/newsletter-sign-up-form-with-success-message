2143500561e2fa39057e19863b2735c03c9685c0
heejung1180 <heejung1180@outlook.com>
2024-03-19 20:03:47 -0700

    inital onclick function for subscribe and dismiss

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function subscribe() {
+  document.getElementById("blockOne").style.display = "none"
+  document.getElementById("containerTwo").style.display = "block"
+}
+
+function dismiss() {
+  document.getElementById("containerTwo").style.display = "none"
+  document.getElementById("blockOne").style.display = "block"
+}
+
+
