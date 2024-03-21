28b7773a479f7e62cb0e6d254a3759438f70c131
heejung1180 <heejung1180@outlook.com>
2024-03-21 08:57:23 -0700

    added function to input email value to success

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
 
 function subscribe() {
+  let bold = document.createElement("b")
+  let inputValue = document.getElementById("email").value;
+  let t = document.createTextNode(inputValue);
+  bold.appendChild(t);
+  document.getElementById("emailText").appendChild(bold);
+  document.getElementById("email").value = "";
   document.getElementById("blockOne").style.display = "none"
   document.getElementById("containerTwo").style.display = "block"
 }
 
 function dismiss() {
+  let emailValue = document.getElementById("emailText");
+  emailValue.removeChild(emailValue.firstElementChild);
   document.getElementById("containerTwo").style.display = "none"
   document.getElementById("blockOne").style.display = "block"
 }
